79c741ebf3ee8ead7123cf2f3ee3f80b99729f6c
Surendra Gullipalli <surendra.gullipalli6@gmail.com>
2024-10-17 17:46:15 +0100

    Implemented reusable TextField component into design system

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,47 +1,20 @@
+import Textfield from "../design-system/Textfield";
+
 const PersonalDetails = () => {
   return (
     <div className="text-black">
       <h2 className="text-lg font-semibold mb-4">Personal Details</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <input
-          type="text"
-          placeholder="First Name"
-          className="border p-2 rounded"
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          className="border p-2 rounded"
-        />
-        <input
-          type="text"
-          placeholder="Phone Number"
-          className="border p-2 rounded"
-        />
-        <input
-          type="email"
-          placeholder="Email ID"
-          className="border p-2 rounded"
-        />
-        <input type="text" placeholder="City" className="border p-2 rounded" />
-        <input
-          type="text"
-          placeholder="Country"
-          className="border p-2 rounded"
-        />
-        <input
-          type="url"
-          placeholder="LinkedIn URL"
-          className="border p-2 rounded"
-        />
-        <input
-          type="url"
-          placeholder="GitHub URL"
-          className="border p-2 rounded"
-        />
+        <Textfield label={"First Name"} />
+        <Textfield label={"Last Name"} />
+        <Textfield label={"Phone Number"} />
+        <Textfield label={"Email ID"} />
+        <Textfield label={"City"} />
+        <Textfield label={"Country"} />
+        <Textfield label={"LinkedIn URL"} />
+        <Textfield label={"GitHub URL"} />
         <input
           type="file"
-          placeholder="Profile Picture"
           className="border p-2 rounded col-span-1 md:col-span-2"
         />
       </div>
---
+++ src/design-system/Textfield.jsx
@@ -0,0 +1,20 @@
+const Textfield = ({ label, styleClasses }) => {
+  return (
+    <label
+      htmlFor={label}
+      className={`${styleClasses} relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600`}
+    >
+      <input
+        type="text"
+        className="w-full p-2 peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
+        placeholder={label}
+      />
+
+      <span className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-500 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs">
+        {label}
+      </span>
+    </label>
+  );
+};
+
+export default Textfield;
